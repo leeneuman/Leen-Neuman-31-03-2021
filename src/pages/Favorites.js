613f82edcabcdfee9b68e7a2600c070fc88671a7
Leen Neuman <leenamromin@gmail.com>
2021-04-02 18:00:56 +0300

    design fixes

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -5,6 +5,10 @@ import FavoriteData from '../components/FavoriteData';
 function Favorites() {
 	const favorites = useSelector((state) => state.favorites);
 
+	if (favorites.length === 0) {
+		return <h2 className="text-center mt-5 text-white">No favorites yet...</h2>;
+	}
+
 	return (
 		<div className="FavoritesContainer d-flex flex-row flex-wrap p-4 w-100">
 			{favorites.map((city) => <FavoriteData key={city.Key} city={city} />)}
