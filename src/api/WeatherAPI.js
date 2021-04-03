b034460c32de9095a79e34052be4b2bf8b4a40a4
Leen Neuman <leenamromin@gmail.com>
2021-04-03 11:52:52 +0300

    api key

--- src/api/WeatherAPI.js
+++ src/api/WeatherAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const BASE_URL = 'https://dataservice.accuweather.com';
-const API_KEY = '3QtNaCFlNvFPE2vCYQ8nMh5C04bTGrjC';
+const API_KEY = 'KpvPAsNLBJJKYWnDQmnsLAjHvQ1c1jKU';
 
 export const getSuggestions = (searchText) => {
 	return axios.get(`${BASE_URL}/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${searchText}`, {
